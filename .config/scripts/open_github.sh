b046ae33d1111dcd5d511e23cea2acfacfe9f5b8
Henri Lemoine <henri123lemoine@gmail.com>
2026-04-17 05:23:10 +0000

    chore(scripts): update open_github.sh

--- .config/scripts/open_github.sh
+++ .config/scripts/open_github.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
-# Open the current repository in the browser
 dir=$(tmux run "echo #{pane_start_path}")
-cd $dir
-url=$(git remote get-url origin) 
+cd "$dir"
+url=$(git remote get-url origin)
 
-# Check if the repository is on GitHub
-if [[ $url == *"github.com"* ]]; then
-  open $url
-else
+if [[ $url != *"github.com"* ]]; then
   echo "This repository is not hosted on GitHub"
+  exit 1
 fi
 
-# This script was shamelessly stolen from https://github.com/SylvanFranklin/.config/blob/main/scripts/open_github.sh. All credit goes to him.
+if command -v open &>/dev/null; then
+  open "$url"
+else
+  encoded=$(printf '%s' "$url" | base64 -w0)
+  seq='\033]1337;SetUserVar=open_url='"$encoded"'\a'
+  if [[ -n "$TMUX" ]]; then
+    printf '\033Ptmux;\033'"$seq"'\033\\' > /dev/tty
+  else
+    printf "$seq" > /dev/tty
+  fi
+fi
+
+# Inspired by https://github.com/SylvanFranklin/.config/blob/main/scripts/open_github.sh
 
